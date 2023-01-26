21a3ca96fd0a84d78a69d316f1615b2974974880
Gokhan Sigircik <gokhansigircik@hotmail.com>
2023-01-26 11:56:07 -0500

    more algos

--- santasNaughtyList1.js
+++ santasNaughtyList1.js
@@ -45,53 +45,49 @@ const expected2 = ["Miss Piggy", "Fozzie Bear"];
 const badHabit3 = "vapes too much";
 const expected3 = [];
 
-/**
- * Finds a list of people whose habits contain the given bad habit.
- * - Time O(?).
- * - Space O(?).
- * @typedef {Object} Person
- * @property {string} firstName
- * @property {string} lastName
- * @property {Array<string>} habits
- * @param {Array<Person>} persons
- * @param {string} badHabit
- * @returns {Array<Person>} The people that have the given bad habit.
- */
-function santasNaughtyList(persons, badHabit) {
-  // your code here
-}
 
-/**
- * Finds a list of people whose habits contain the given bad habit.
- * - Time O(?).
- * - Space O(?).
- * @typedef {Object} Person
- * @property {string} firstName
- * @property {string} lastName
- * @property {Array<string>} habits
- * @param {Array<Person>} persons
- * @param {string} badHabit
- * @returns {Array<Person>} The people that have the given bad habit.
- */
-
-// ********1st solution************
-// function santasNaughtyListFunctional(persons, badHabit) {
-//   // your code here
-//   const expected = persons.filter((person) => person.habits.includes(badHabit));
-  
-//   const naughtyList = expected.map((person) => `${person.firstName} ${person.lastName}`);
-//   return naughtyList;
+// function santasNaughtyList(persons, badHabit) {
+//   let expected = []
+//   persons.map (person => {
+//     if (person.habits.includes(badHabit)) {
+//         let fullname = person.firstName + ' ' + person.lastName 
+//         expected.push (fullname) 
+//     } 
+//   })
+//   return expected
 // }
+
+// console.log(santasNaughtyList(students,badHabit3));
+// console.log(santasNaughtyList(students,badHabit1));
+// console.log(santasNaughtyList(students,badHabit2));
+/**
+ * Finds a list of people whose habits contain the given bad habit.
+ * - Time O(?).
+ * - Space O(?).
+ * @typedef {Object} Person
+ * @property {string} firstName
+ * @property {string} lastName
+ * @property {Array<string>} habits
+ * @param {Array<Person>} persons
+ * @param {string} badHabit
+ * @returns {Array<Person>} The people that have the given bad habit.
+ */
+
+// *********another way of solving*****************
 
 
 function santasNaughtyListFunctional(persons, badHabit) {
-  return persons
-    .filter((person) => person.habits.includes(badHabit))
-    .map((person) => `${person.firstName} ${person.lastName}`);
+  let expected = []  // array to push everything into
+  for (let i = 0; i < persons.length;i++) {    // this goes through array
+    for (let j = 0;j < persons[i].habits.length; j++) {  // this goes through the habits of the object
+      if (persons[i].habits[j] === badHabit) {        // this compares the habits to bad habits
+        let fullName = persons[i].firstName + ' ' + persons[i].lastName      // creates var full name and adds names that meet the condition
+        expected.push (fullName)                   // pushes full name to expected array
+      }
+    }
+  }
+  return expected                  // returns expected array 
 }
-
-console.log(santasNaughtyListFunctional(students, badHabit1));
-console.log(santasNaughtyListFunctional(students, badHabit2));
-console.log(santasNaughtyListFunctional(students, badHabit3));
-
-
+console.log(santasNaughtyListFunctional(students,badHabit1));
+console.log(santasNaughtyListFunctional(students,badHabit2));
+console.log(santasNaughtyListFunctional(students,badHabit3));
